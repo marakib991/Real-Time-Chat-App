99d18ed2c2e4669f1bed4a0fe16c1742e79bfda8
marakib991 <rarefigure@gmail.com>
2024-03-07 01:16:25 -0500

    server is connected to mongoDB

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,11 @@
+export const signup = async (req, res) => {
+  try{
+    const {fullName, username, password, confirmPasword, gender }= req.body;
+  }catch(err){
+
+  }
+}
+
 export const login = (req, res) => {
   console.log("login");
 }
@@ -6,6 +14,3 @@ export const logout = (req, res) => {
   console.log("logout");
 }
 
-export const signup = (req, res) => {
-  console.log("signup");
-}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
  const app = express();
 
@@ -15,4 +16,7 @@ import authRoutes from "./routes/auth.routes.js";
 
 app.use("/api/auth", authRoutes)
 
- app.listen(PORT, ()=> console.log(`Server is Running on Port ${PORT}`));
+app.listen(PORT, ()=> {
+  connectToMongoDB();
+  console.log(`Server is Running on Port ${PORT}`);
+});
