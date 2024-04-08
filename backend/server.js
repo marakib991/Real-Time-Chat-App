011e24cec48c648d5c154d3031b155a79d3896c7
marakib991 <rarefigure@gmail.com>
2024-04-08 03:04:07 -0400

    get users for sidebar endpoint is commited

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
  const app = express();
@@ -15,12 +18,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
  app.use("/api/auth", authRoutes);
  app.use("/api/messages", messageRoutes)
+ app.use("/api/users", userRoutes);
 
-//  app.get("/", (req, res)=>{
-//   //root route 
-//   res.send("Server is ready!!");
-//  });
- 
+
  app.listen(PORT, ()=> {
   connectToMongoDB();
   console.log(`Server is Running on Port ${PORT}`);
