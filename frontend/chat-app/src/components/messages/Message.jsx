c72892a274796d6c98ec247dc7deffa2b3184324
marakib991 <rarefigure@gmail.com>
2024-04-12 02:21:58 -0400

    UI design is commited

---
+++ frontend/chat-app/src/components/messages/Message.jsx
@@ -0,0 +1,21 @@
+const Message = ({ message }) => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src={
+              "https://yt3.googleusercontent.com/ytc/AIdro_k2wsQa2j9sAhjS25DyZxrhAGDJWtNZBYcLVd3uqQ=s900-c-k-c0x00ffffff-no-rj"
+            }
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white bg-blue-500 pb-2`}>Hi...!!!</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        12:42
+      </div>
+    </div>
+  );
+};
+export default Message;
