2b3f3720fe3724a5b1ede066d2c85f9c86edc1e3
marakib991 <rarefigure@gmail.com>
2024-05-04 00:39:41 -0400

    implemented socket.io is commited

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
- const app = express();
+
  const PORT = process.env.PORT || 5700;
 
  dotenv.config();
@@ -21,7 +23,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
  app.use("/api/users", userRoutes);
 
 
- app.listen(PORT, ()=> {
+ server.listen(PORT, ()=> {
   connectToMongoDB();
   console.log(`Server is Running on Port ${PORT}`);
 });
